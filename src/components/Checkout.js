dcd9a0250e891d9b818e98b5e000284d151f2384
onjorobrian <onjorobrian1@gmail.com>
2023-11-14 10:15:12 +0300

    componentcommit

--- src/components/Checkout.js
+++ src/components/Checkout.js
@@ -1,25 +1,6 @@
 import { useEffect,useState } from "react";
 
-function Checkout(){
-    const [details ,setDetails] =useState([])
-    useEffect(()=>{
-        fetch("https://json-server-dusky-nine.vercel.app/order-details")
-        .then((response)=> response.json())
-        .then(data => setDetails(data))
-    },[])
 
-    function handleTransactionApproved(){
-        alert("trasaction approved")
-        
-    }
-    function handleTransactionDeclined(id){
-        console.log(id)
-        fetch(`https://json-server-dusky-nine.vercel.app/order-details/${id}`,{
-            method:"DELETE"
-        })
-        const updated = details.filter((item) => item.id !== id)
-        setDetails(updated)
-    }
     return(
         <table>
             <thead>
