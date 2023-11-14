5bf5126b4cadf982212ac33d4e030efa2f61f6a7
onjorobrian <onjorobrian1@gmail.com>
2023-11-14 10:32:42 +0300

    componentcommit

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -26,20 +26,7 @@ function Login(){
    
 
     return(
-        <form onSubmit={handleSubmit}>
-        <label>
-          Email:
-          <input type="text" value={getEmail} onChange={(e) => setEmail(e.target.value)} />
-        </label>
-        <br />
-        <label>
-          Password:
-          <input type="password" value={getPassword} onChange={(e) => setPassword(e.target.value)} />
-        </label>
-        <br />
-        <button type="submit">Login</button>
-        <h4>Register Here<Link to="/registration">Register</Link></h4>
-      </form>
+       
     )
 }
 export default Login;
