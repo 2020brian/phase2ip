1c2291c8a0a0d959af46863477a98b4d90617d3a
onjorobrian <onjorobrian1@gmail.com>
2023-11-14 10:24:55 +0300

    componentcommit

--- src/components/LoginPage.js
+++ src/components/LoginPage.js
@@ -1,7 +1,27 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 
-
+function Login(){
+    const [getEmail, setEmail] = useState('')
+    const [getPassword, setPassword] = useState('')
+    //const [isLoggedIn,setIsLoggedIn]= useState(false)
+    const [userdata, setUserData]= useState([])
+    useEffect(()=>{
+        fetch('https://json-server-dusky-nine.vercel.app/user-data')
+        .then(response => response.json())
+        .then( data => setUserData(data))
+    },[])
+    const user = userdata.find((user) => user.email === getEmail && user.password === getPassword)
+    function handleSubmit(e){
+        e.preventDefault()
+            if(user){
+                alert('Login Success')
+            }else{
+               alert('Wrong password and email combination')
+            }
+            setEmail('')
+            setPassword('')
+    }
       
    
 
